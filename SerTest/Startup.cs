508e652ec59009fe45809439edba7574ed3d63d1
homxuwang <335103133@qq.com>
2017-12-27 10:04:06 +0800

    添加项目文件。

---
+++ SerTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SerTest.Startup))]
+namespace SerTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
